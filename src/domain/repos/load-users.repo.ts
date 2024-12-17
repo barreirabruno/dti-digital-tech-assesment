dcc5ec46166e061abe3929c820e981bd7ddc314b
Bruno  Barreira <barreiradev@gmail.com>
2024-12-17 00:25:43 -0300

    feat: add LoadJSONPlaceholderUsersGateway

--- src/domain/repos/load-users.repo.ts
+++
@@ -1,7 +0,0 @@
-import type UserDomainEntity from '@/domain/entities/user.domain.entity'
-
-export default interface LoadUsersRepository {
-  all: () => Promise<LoadUsersOutput>
-}
-
-export type LoadUsersOutput = UserDomainEntity[]
